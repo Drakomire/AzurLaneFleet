b01ec088ac89af259d502dc9deb158ae3c7712a9
Rik Jilleba <79363708+etheralda@users.noreply.github.com>
2021-05-30 19:19:19 +0200

    fixed stat toggle and stats suming

--- client/scripts/reworkVue.js
+++ client/scripts/reworkVue.js
@@ -12,7 +12,7 @@ Vue.component("fleet-container", {
     template: `
         <div class="fleetWrap" v-bind:id="fleet.id">
 
-            <input class="DetailToggle" type="checkbox" name="details" v-bind:id="fleet.id">
+            <input class="DetailToggle" type="checkbox" name="details" v-bind:id="fleet.name">
 
             <div class="fleetHeader">
 
@@ -193,12 +193,15 @@ Vue.component("shipwrap-stat",{
                 v-if="(typeof(statRetro)=='number')"
                 >{{statRetro}}</div>
             </div>
+
             <div class="totalStat"
-            v-if="(typeof(statValue)=='number')"
-            >{{statValue}}</div>
-            <div class="totalStat"
-            v-if="(typeof(statValue)=='string')"
-            >{{(statEquip!==null)?statEquip:(statRetro!==null)?statRetro:statValue}}</div>
+                v-if="(typeof(statValue)==='number')"
+                v-text="statValue+((statEquip!=null&&statEquip>0)?statEquip:0)+((statRetro!==null&&statRetro>0)?statRetro:0)"
+            ></div>
+
+            <div class="totalStat" v-else
+            >{{((statEquip&&statEquip!==null)?statEquip:((statRetro&&statRetro!==null)?statRetro:statValue))}}</div>
+
         </div>
     </div>
     `
@@ -235,3 +238,58 @@ Vue.component("ship-rarity-button", {
         ></button>
     `
 });
+
+// popup menu components
+vue.component("popup-ships", {
+    props:{
+        fleet:Object,
+        lang:String,
+    },
+    template:`
+    <div class="modal fade" id="shipselect">
+        <div class="modal-dialog">
+            <div class="modal-content text-light bg-dark">
+                <div class="modal-header">
+                    <h4 class="modal-title">Select Ship</h4>
+                    <button type="button" class="close text-light" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="container">
+                        <div class="container">Faction</div>
+                        <div class="container" id='shipnation'>
+                            <ship-nation-button v-for="item in nation" v-bind:key="item.id" v-bind:name="item.name" v-text="item[lang]" v-bind:value="item.id"></ship-nation-button>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="container">Hull Class</div>
+                        <div class="container" id="shiptype">
+                            <ship-type-button v-for="item in type" v-bind:key="item.id" v-bind:name="item.name" v-show="item.display" v-text="item[lang]" v-bind:value="item.id"></ship-type-button>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="container">Rarity</div>
+                        <div class="container" id='shiprarity'>
+                            <ship-rarity-button v-for="item in rarity" v-bind:key="item.id" v-bind:name="item.name" v-text="item[lang]" v-bind:value="item.id"></ship-rarity-button>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <!-- <div class="container">只顯示改造後的船 / 改造完了のみ / Retrofitted Ship Only</div> -->
+                        <!-- <div class="container">
+                            <button id='shipretro' name="shipretro" type="button" class="btn btn-outline-light btn-sm active" aria-pressed="true" value="1" onclick="setRetro(this)">Yes</button>
+                        </div> -->
+                                            <div class="container">
+                        <div>Name</div>
+                        <input type="text" placeholder="Enter ship name..." id="ship search bar" onkeyup="updateSearch(this)">
+                      </div>
+                    </div>
+                    <spacer type="horizontal" width="100" height="8">⠀</spacer>
+                    <div class="container-fluid">
+                        <div class="container">　　　</div>
+                    </div>
+                    <div class="container-fluid row m-0 p-0" id="shiplist"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+})
