43fee9f798a2513d0d216451db3d9fca887dd9a3
Rik Jilleba <79363708+etheralda@users.noreply.github.com>
2021-06-16 09:46:49 +0200

    data transfer push

--- client/scripts/test/json converter.js
+++ client/scripts/test/json converter.js
@@ -1,4 +1,4 @@
-const shipJsonIn = Object;
+// const shipJsonIn = Object;
 const equipJsonIn = Object;
 
 var shipJsonOut = {}
@@ -9,65 +9,61 @@ function sortShipData(){
         let tempShip = {}
         //set difrent Ship names values
         tempShip.name={
-            "cn":ship.CNname,
-            "jp":ship.JPname,
+            "cn":JSON.parse(ship.ship.name.cn),
+            "jp":JSON.parse(ship.ship.name.jp),
             "en":ship?.ENname??parseCode(ship.code),
             "code":ship.code,
         }
         //set ship base stats
         tempShip.stats={
-            "health": ship.hp,
-            "armor": ship.armor,
-            "reload": ship.reload,
-            "luck": ship.luck,
-            "firepower": ship.fp,
-            "torpedo": ship.trp,
-            "evasion": ship.eva,
-            "speed": ship.spd,
-            "antiair": ship.aa,
-            "aviation": ship.avi,
-            "oilConsumption": ship.cost,
-            "accuracy": ship.acc,
-            "antisubmarineWarfare": ship.asw
+            "health": ship.ship.data[`${ship.id}1`].stats.hp,
+            "armor": GetFromDict(armorDict,ship.ship.armor,lang??"en"),
+            "reload": ship.ship.data[`${ship.id}1`].stats.rld,
+            "luck": ship.ship.data[`${ship.id}1`].stats.luk,
+            "firepower": ship.ship.data[`${ship.id}1`].stats.fp,
+            "torpedo": ship.ship.data[`${ship.id}1`].stats.trp,
+            "evasion": ship.ship.data[`${ship.id}1`].stats.eva,
+            "speed": ship.ship.data[`${ship.id}1`].stats.spd,
+            "antiair": ship.ship.data[`${ship.id}1`].stats.aa,
+            "aviation": ship.ship.data[`${ship.id}1`].stats.avi,
+            "oilConsumption": `${ship.ship.data[`${ship.id}1`].stats.oil} - ${ship.ship.data[`${ship.id}4`].stats.oil}`,
+            "accuracy": ship.ship.data[`${ship.id}1`].stats.acc,
+            "antisubmarineWarfare": ship.ship.data[`${ship.id}1`].stats.asw
         }
         // set ship extradata values
         tempShip.extraData={
             "shipID":ship.Uid,
             "rarity":{
-                "string":ship.rarity,
-                "numeric":null,
+                "string":"",
+                "numeric":ship.ship.data[`${ship.id}1`].rarity,
             },
-            "type":ship.type,
+            "type":ship.ship.type,
             "growth":{
                 "100":{
-                    "health": ship.hpGrowth100,
-                    "armor": ship.armorGrowth100,
-                    "reload": ship.reloadGrowth100,
-                    "luck": ship.luckGrowth100,
-                    "firepower": ship.fpGrowth100,
-                    "torpedo": ship.trpGrowth100,
-                    "evasion": ship.evaGrowth100,
-                    "speed": ship.spdGrowth100,
-                    "antiair": ship.aaGrowth100,
-                    "aviation": ship.aviGrowth100,
-                    "oilConsumption": ship.costGrowth100,
-                    "accuracy": ship.accGrowth100,
-                    "antisubmarineWarfare": ship.aswGrowth100
+                    "health": ship.ship.data[`${ship.id}1`].stats_growth.hp,
+                    "reload": ship.ship.data[`${ship.id}1`].stats.rld,
+                    "luck": ship.ship.data[`${ship.id}1`].stats_growth.luk,
+                    "firepower": ship.ship.data[`${ship.id}1`].stats_growth.fp,
+                    "torpedo": ship.ship.data[`${ship.id}1`].stats_growth.trp,
+                    "evasion": ship.ship.data[`${ship.id}1`].stats_growth.eva,
+                    "speed": ship.ship.data[`${ship.id}1`].stats_growth.spd,
+                    "antiair": ship.ship.data[`${ship.id}1`].stats_growth.aa,
+                    "aviation": ship.ship.data[`${ship.id}1`].stats_growth.avi,
+                    "accuracy": ship.ship.data[`${ship.id}1`].stats_growth.acc,
+                    "antisubmarineWarfare": ship.ship.data[`${ship.id}1`].stats_growth.asw
                 },
                 "120":{
-                    "health": ship.hpGrowth120,
-                    "armor": ship.armorGrowth120,
-                    "reload": ship.reloadGrowth120,
-                    "luck": ship.luckGrowth120,
-                    "firepower": ship.fpGrowth120,
-                    "torpedo": ship.trpGrowth120,
-                    "evasion": ship.evaGrowth120,
-                    "speed": ship.spdGrowth120,
-                    "antiair": ship.aaGrowth120,
-                    "aviation": ship.aviGrowth120,
-                    "oilConsumption": ship.costGrowth120,
-                    "accuracy": ship.accGrowth120,
-                    "antisubmarineWarfare": ship.aswGrowth120
+                    "health": ship.ship.data[`${ship.id}4`].stats_growth_extra.hp                   ??ship.ship.data[`${ship.id}1`].stats_growth_extra.hp,
+                    "reload": ship.ship.data[`${ship.id}4`].stats_growth_extra.rld                  ??ship.ship.data[`${ship.id}1`].stats_growth_extra.rld,
+                    "luck": ship.ship.data[`${ship.id}4`].stats_growth_extra.luk                    ??ship.ship.data[`${ship.id}1`].stats_growth_extra.luk,
+                    "firepower": ship.ship.data[`${ship.id}4`].stats_growth_extra.fp                ??ship.ship.data[`${ship.id}1`].stats_growth_extra.fp,
+                    "torpedo": ship.ship.data[`${ship.id}4`].stats_growth_extra.trp                 ??ship.ship.data[`${ship.id}1`].stats_growth_extra.trp,
+                    "evasion": ship.ship.data[`${ship.id}4`].stats_growth_extra.eva                 ??ship.ship.data[`${ship.id}1`].stats_growth_extra.eva,
+                    "speed": ship.ship.data[`${ship.id}4`].stats_growth_extra.spd                   ??ship.ship.data[`${ship.id}1`].stats_growth_extra.spd,
+                    "antiair": ship.ship.data[`${ship.id}4`].stats_growth_extra.aa                  ??ship.ship.data[`${ship.id}1`].stats_growth_extra.aa,
+                    "aviation": ship.ship.data[`${ship.id}4`].stats_growth_extra.avi                ??ship.ship.data[`${ship.id}1`].stats_growth_extra.avi,
+                    "accuracy": ship.ship.data[`${ship.id}4`].stats_growth_extra.acc                ??ship.ship.data[`${ship.id}1`].stats_growth_extra.acc,
+                    "antisubmarineWarfare": ship.ship.data[`${ship.id}4`].stats_growth_extra.asw    ??ship.ship.data[`${ship.id}1`].stats_growth_extra.asw
                 },
                 "kai":{
                     //retroTileData
@@ -75,6 +71,7 @@ function sortShipData(){
             }
         },
         //set empty equip slots
+        tempShip.slots=ship.ship.slots,
         tempShip.items=[{},{},{},{},{}]
         //set icon bacground and border img source
         ship.extraData.icon=`client/shipicon/${ship.icon}`,// TODO need file extention?
@@ -85,6 +82,8 @@ function sortShipData(){
         shipJsonOut[ship.Uid]=tempShip
     })
 }
+
+base+lvl100bonus*99+lvl120*19
 
 function sortEquipData(){
     equipJsonIn.forEach(equip=>{
--- client/scripts/test/staticDictionary.js
+++ client/scripts/test/staticDictionary.js
@@ -2,7 +2,7 @@ const shipTypeDict = [
     {
         "index":["Destroyer",1,"DD","驅逐","驅逐"],
         "numeric":1,
-        "string":"Destroyer",
+        "en":"Destroyer",
         "code":"DD",
         "cn":"驅逐",
         "jp":"驅逐"
@@ -10,7 +10,7 @@ const shipTypeDict = [
     {
         "index":["Light Cruiser",2,"CL","輕巡","軽巡"],
         "numeric":2,
-        "string":"Light Cruiser",
+        "en":"Light Cruiser",
         "code":"CL",
         "cn":"輕巡",
         "jp":"軽巡"
@@ -18,7 +18,7 @@ const shipTypeDict = [
     {
         "index":["Heavy Cruiser",3,"CA","重巡","重巡"],
         "numeric":3,
-        "string":"Heavy Cruiser",
+        "en":"Heavy Cruiser",
         "code":"CA",
         "cn":"重巡",
         "jp":"重巡"
@@ -26,7 +26,7 @@ const shipTypeDict = [
     {
         "index":["Large Cruiser",18,"CB","超巡","超甲巡"],
         "numeric":18,
-        "string":"Large Cruiser",
+        "en":"Large Cruiser",
         "code":"CB",
         "cn":"超巡",
         "jp":"超甲巡"
@@ -34,7 +34,7 @@ const shipTypeDict = [
     {
         "index":["Submarine",8,"SS","潛艇","潜水艦"],
         "numeric":8,
-        "string":"Submarine",
+        "en":"Submarine",
         "code":"SS",
         "cn":"潛艇",
         "jp":"潜水艦"
@@ -42,7 +42,7 @@ const shipTypeDict = [
     {
         "index":["Submarine Carrier",17,"SSV","潛母","潜水空母"],
         "numeric":17,
-        "string":"Submarine Carrier",
+        "en":"Submarine Carrier",
         "code":"SSV",
         "cn":"潛母",
         "jp":"潜水空母"
@@ -50,7 +50,7 @@ const shipTypeDict = [
     {
         "index":["Battlecruiser",4,"BC","戰巡","巡洋戦艦"],
         "numeric":4,
-        "string":"Battlecruiser",
+        "en":"Battlecruiser",
         "code":"BC",
         "cn":"戰巡",
         "jp":"巡洋戦艦"
@@ -58,7 +58,7 @@ const shipTypeDict = [
     {
         "index":["Battleship",5,"BB","戰列","戦艦"],
         "numeric":5,
-        "string":"Battleship",
+        "en":"Battleship",
         "code":"BB",
         "cn":"戰列",
         "jp":"戦艦"
@@ -66,7 +66,7 @@ const shipTypeDict = [
     {
         "index":["Light Carrier",6,"CVL","輕航","軽空母"],
         "numeric":6,
-        "string":"Light Carrier",
+        "en":"Light Carrier",
         "code":"CVL",
         "cn":"輕航",
         "jp":"軽空母"
@@ -74,7 +74,7 @@ const shipTypeDict = [
     {
         "index":["Carrier",7,"CV","航母","空母"],
         "numeric":7,
-        "string":"Carrier",
+        "en":"Carrier",
         "code":"CV",
         "cn":"航母",
         "jp":"空母"
@@ -82,7 +82,7 @@ const shipTypeDict = [
     {
         "index":["Monitor",13,"BM","重砲","砲艦"],
         "numeric":13,
-        "string":"Monitor",
+        "en":"Monitor",
         "code":"BM",
         "cn":"重砲",
         "jp":"砲艦"
@@ -90,7 +90,7 @@ const shipTypeDict = [
     {
         "index":["Repair Ship",12,"AR","維修","工作"],
         "numeric":12,
-        "string":"Repair Ship",
+        "en":"Repair Ship",
         "code":"AR",
         "cn":"維修",
         "jp":"工作"
@@ -98,22 +98,93 @@ const shipTypeDict = [
     {
         "index":["Other",0,"NaP","其他","その他"],
         "numeric":0,
-        "string":"Other",
+        "en":"Other",
         "code":"NaP",
         "cn":"其他",
         "jp":"その他"
     },
+]
+
+const equipTypeDict = []
+
+const nationalDict = []
+
+const armorDict = [
+    {
+        "index":["light",1,"",""],
+        "numeric":1,
+        "en":"light",
+        "cn":"",
+        "jp":""
+    },
+    {
+        "index":["medium",2,"",""],
+        "numeric":2,
+        "en":"medium",
+        "cn":"",
+        "jp":""
+    },
+    {
+        "index":["heavy",3,"",""],
+        "numeric":3,
+        "en":"heavy",
+        "cn":"",
+        "jp":""
+    },
+
+]
+
+const rartityDict = [
+    {
+        "index":[2,"common","C"],
+        "numeric":2,
+        "en":"common",
+        "enCode":"C",
+        "cn":"",
+        "jp":""
+    },
+    {
+        "index":[3,"rare","R"],
+        "numeric":3,
+        "en":"rare",
+        "enCode":"R",
+        "cn":"",
+        "jp":""
+    },
+    {
+        "index":[4,"elite","E"],
+        "numeric":4,
+        "en":"elite",
+        "enCode":"E",
+        "cn":"",
+        "jp":""
+    },
+    {
+        "index":[5,"super rare","SR","SSR"],
+        "numeric":5,
+        "en":"super rare",
+        "enCode":"SR",
+        "cn":"",
+        "jp":""
+    },
+    {
+        "index":[6,"ultra rare","UR"],
+        "numeric":6,
+        "en":"ultra rare",
+        "enCode":"UR",
+        "cn":"",
+        "jp":""
+    },
+]
+
     // {
     //     "index":["",,"","",""],
     //     "numeric":,
-    //     "string":"",
+    //     "en":"",
     //     "code":"",
     //     "cn":"",
     //     "jp":""
     // },
-]
-
-
 function GetFromDict(dictToUse,SearchFor,ReturnAlt){
     dictToUse.forEach(entry=>{
         if(entry.index.includes(SearchFor)){
