94622fc7d6d3b0cfdfe6398c93a63f4fa349f8ba
Rik Jilleba <79363708+etheralda@users.noreply.github.com>
2021-06-16 11:56:04 +0200

    data transfer push

--- client/scripts/debugin/Test.js
+++ client/scripts/debugin/Test.js
@@ -1,81 +1,52 @@
-const defaultShip ={
-    "name":{
-        "cn":"",
-        "jp":"",
-        "en":""
-    },
-    "stats":{
-        "health": 0,
-        "armor": "none",
-        "reload": 0,
-        "luck": 0,
-        "firepower": 0,
-        "torpedo": 0,
-        "evasion": 0,
-        "speed": 0,
-        "antiair": 0,
-        "aviation": 0,
-        "oilConsumption": 0,
-        "accuracy": 0,
-        "antisubmarineWarfare": 0
-    },
-    "extraData":{
-        "rarity":"",
-        "icon":"",
-        "type":"",
-        "equipBonus":{
-        }
-    },
-    "items":[{
-    },{
-    },{
-    },{
-    },{
-    }]
-}
-const defaultEquip ={
-    "ID":-1,
-    "Name":{
-        "en":"none",
-    },
-    "Type":-1,
-    "Property":{},
-}
-function newFleet(type,fleets=[]) {
-    let fleet = {
-        "id":fleets.length,
-        "name":`${fleets.length+1}`,
-    }
-    if(type=="surface"){
-        fleet.surface = {
-            flagship:defaultShip,
-            leftflank:defaultShip,
-            rightflank:defaultShip,
-            vanguardLead:defaultShip,
-            vanguardMid:defaultShip,
-            vanguardBack:defaultShip,
-        }
-    }else if(type=="subs"){
-        fleet.subs = {
-            flagSub:defaultShip,
-            leftSub:defaultShip,
-            rightSub:defaultShip,
-        }
-    }
-    for(let prop in fleet){
-        if(typeof fleet[prop] =="object"){
-            console.log(prop)
-            for(let ship in fleet[prop]){
-                for(let index=0;index<fleet[prop][ship].items.length;index++){
-                    fleet[prop][ship].items[index]=defaultEquip
-                }
-            }
-        }
-    }
-    return fleet
-}
-var fleets=[]
+var damageData = [
+    [
+        [5,30],
+        [32,300]
+        [25,150],
+    ],
+    [
+        [6,40],
+        [1.5,12],
+        [24,240]
+    ],
+    [
+        [4,25],
+        [8,50],
+        [26,160]
+    ],
+    [
+        [18,300],
+        [4,25],
+        [19,150]
+    ],
+    [
+        [22,270],
+        [4,25],
+        [23,125]
+    ],
+    [
+        [20,240],
+        [4,25],
+        [21,100]
+    ]
+]
+var baseOffsetX = 50
+var baseOffsetY = 300
+var frstRn = true
 
-var test1 = ()=>{console.log(fleets)};
-var test2 = ()=>{fleets.push(newFleet("surface",fleets))};
-var test3 = ()=>{fleets.push(newFleet("subs",fleets))};
+const canvas = document.getElementById("dpsgraph")
+const ctx = canvas.getContext("2d")
+
+function drawgraph(damageData){
+    damageData.forEach(datapoint=>{
+        datapoint.forEach(damageloop=>{
+            ctx.beginPath()
+            ctx.moveTo(baseOffsetX,baseOffsetY)
+            let damageCount = 0
+            for(let i= damageloop[0];i<120;i+damageloop[0]){
+                ctx.lineTo(baseOffsetX+i,baseOffsetY-)
+            }
+        })
+    })
+}
+function drawlegnd()
